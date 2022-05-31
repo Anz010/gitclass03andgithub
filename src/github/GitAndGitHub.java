9315e55f1bdef576fcffd6b282dbeabd98929bd3
Anz010 <anzarovanzor@gmail.com>
2022-05-31 19:54:36 -0400

    3rd commit

--- src/github/GitAndGitHub.java
+++ src/github/GitAndGitHub.java
@@ -17,6 +17,18 @@ public class GitAndGitHub {
 			System.out.println();
 		}
 		
+		for (int x = 1; x < 8; x++) {
+			for (int y = 1; y <= 8 - x; y++) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
+		}
+		for (int x = 1; x < 7; x++) {
+			for (int y = 1; y < x + 2; y++) {
+				System.out.print(y + " ");
+			}
+			System.out.println("Today is a very good weather");
+		}
 		
 		
 		
