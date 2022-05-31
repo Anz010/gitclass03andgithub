91206a90533bc264a4e5de4d167db44a6d3b4de6
Anz010 <anzarovanzor@gmail.com>
2022-05-31 19:37:04 -0400

    2nd commit

---
+++ src/github/GitAndGitHub.java
@@ -0,0 +1,29 @@
+package github;
+
+public class GitAndGitHub {
+
+	public static void main(String[] args) {
+		
+		for (int x = 1; x < 8; x++) {
+			for (int y = 1; y <= 8 - x; y++) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
+		}
+		for (int x = 1; x < 7; x++) {
+			for (int y = 1; y < x + 2; y++) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
