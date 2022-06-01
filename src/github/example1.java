86a0dd3ca89ee5bd7aff917167ac8583932a1f45
Anz010 <anzarovanzor@gmail.com>
2022-05-31 21:06:06 -0400

    4rd commit

---
+++ src/github/example1.java
@@ -0,0 +1,12 @@
+package github;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		for (int i=0;i<7;i++) {
+			for(int j=1;j<3;j++) {
+		
+		System.out.println("Hello world");}}
+	}
+
+}
